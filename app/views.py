0e30f6c9b97e65100ae77a9b2ea9c82dbed9705f
gabrielCLimaa <gabrielcrcl12345@gmail.com>
2022-01-19 13:25:18 -0300

    Tickets running

--- app/views.py
+++ app/views.py
@@ -1,22 +1,27 @@
-from django.shortcuts import render , redirect
+from django.shortcuts import render , redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
+from django.http import HttpResponse
 from .forms import *
 from .models import *
 from django.utils import timezone
 import datetime
 
 # Create your views here.
+VEICULO_JA_ESTACIONADO = -1
+VEICULO_PRONTO = 1
 
 def index(request):
     vagas = Vaga.objects.count()
     carros = Carro.objects.count()
     tickets = Ticket.objects.count()
+    vagas_disponiveis = Vaga.objects.filter(carro=None).count()
 
     context = {
         'vagas' : vagas,
         'carros' : carros,
-        'tickets' : tickets
+        'tickets' : tickets,
+        'vagas_disponiveis' : vagas_disponiveis
     }
 
     return render(request, 'index.html', context=context)
@@ -27,48 +32,21 @@ class VagaListView(generic.ListView):
 class VagaDetailView(generic.DetailView):
     model = Vaga
 
-def cacularTicket(horaSaida, placa):
-    ticket = Ticket.objects.get(placaVeiculo=placa,valor=0)
-    horario = horaSaida- ticket.horaDeEntrada
-
-    if horario < datetime.timedelta(hours=1):
-        ticket.valor = 15
-
-    if horario > datetime.timedelta(hours=1):
-        ticket.valor = 25
-
-    if horario > datetime.timedelta(hours=3):
-        ticket.valor = 45
-
-    if horario > datetime.timedelta(hours=6):
-        ticket.valor = 55
-
-    ticket.horaDeSaida = horaSaida
-    ticket.save()
-
-def TriggerCreateTicket(placa):
-    ticket = Ticket(
-        placaVeiculo = str(placa),
-        valor = 0
-    )
-    ticket.save()
-
-
 def VagaCriar(request):
     if request.method == 'POST':
         form = VagaForm(request.POST)
         if form.is_valid():
             vaga = form.save(commit=False)
 
-            if vaga.carro.placa:
-                TriggerCreateTicket(str(vaga.carro.placa))
+            if vaga.carro != None:
+                if vaga.carro.placa:
+                    TriggerCreateTicket(str(vaga.carro.placa))
 
             vaga.save()
             return redirect('vagas')
 
     form = VagaForm
     return render(request, 'vaga_criar.html',{'form': form})
-
 
 def VagaAtualizar(request,pk):
     if request.method == 'POST':
@@ -88,6 +66,8 @@ def VagaAtualizar(request,pk):
                     cacularTicket(timezone.now(), placaParaTicketDeSaida)
 
             if vagaForm.carro != None:
+                if checarVagas(vagaForm.carro.placa) == VEICULO_JA_ESTACIONADO:
+                    return render(request, 'error/erro_vaga_criar.html')
                 TriggerCreateTicket(str(vagaForm.carro.placa))
 
             vagaForm.save()
@@ -96,10 +76,58 @@ def VagaAtualizar(request,pk):
     form = VagaForm()
     return render(request, 'vaga_atualizar.html',{'form': form})
 
-class VagaDelete(generic.DeleteView):
-    model = Vaga
-    success_url = reverse_lazy('vagas')
+def checarVagas(placa):
+    carro = Carro.objects.get(placa=placa)
+    vagas = Vaga.objects.filter(carro=carro)
 
+    if vagas.exists():
+        return VEICULO_JA_ESTACIONADO
+    else:
+        return VEICULO_PRONTO
+
+def TriggerCreateTicket(placa):
+    ticket = Ticket(
+        placaVeiculo = str(placa),
+        valor = 0
+    )
+    ticket.save()
+
+def cacularTicket(horaSaida, placa):
+    ticket = Ticket.objects.get(placaVeiculo=placa,valor=0)
+    horario = horaSaida - ticket.horaDeEntrada
+    ticket.valor = calcularValor(horario)
+    ticket.horaDeSaida = horaSaida
+    ticket.save()
+
+def calcularValor(horarioDaEstadia):
+    if horarioDaEstadia < datetime.timedelta(hours=1):
+        return 15
+
+    if horarioDaEstadia > datetime.timedelta(hours=1):
+        return 25
+
+    if horarioDaEstadia > datetime.timedelta(hours=3):
+        return 45
+
+    if horarioDaEstadia > datetime.timedelta(hours=6):
+        return 55
+
+def contemVeiculo(vaga):
+    if vaga.carro == None:
+        return False
+    else: 
+        return True
+
+def VagaDeletar(request,pk):
+    vaga = Vaga.objects.get(pk=pk)
+
+    if contemVeiculo(vaga):
+        return render(request, 'error/erro_vaga_deletar.html')
+    
+    vaga.delete()
+
+    return redirect('vagas')
+    
 class CarroListView(generic.ListView):
     model = Carro
     
