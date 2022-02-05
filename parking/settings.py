956e0f6457dc07fd80b2823ddd51a7c9f9892adf
gabrielCLimaa <gabrielcrcl12345@gmail.com>
2022-02-05 18:09:10 -0300

    Fix

--- parking/settings.py
+++ parking/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-1#r7#1ndv9lvduabyi6flmrb@b^t-dm$h%1eli@&*6rn^!jb)s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 # Application definition
 
